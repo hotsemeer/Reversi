94975a7278c1fccb6ac5f524efaf73541a839006
hotsemeer <32363461+hotsemeer@users.noreply.github.com>
2023-12-18 11:29:59 +0100

    front-end

---
+++ Reversi/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Reversi.Controllers
+{
+    public class UserController : Controller
+    {
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost("login")]
+        public IActionResult LoginUser()
+        {
+            return View();
+        }
+
+
+        [Route("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Reversi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Reversi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
